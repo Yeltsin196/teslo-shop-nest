aedc208b344b6eebf441136df20bff46f86f202a
Yeltsin <yel.ruben@gmail.com>
2026-06-28 15:59:00 -0400

    refactor(app): reorganize imports and enhance configuration for clarity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,39 +1,49 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
 import { ProductsModule } from './products/products.module';
 import { CommonModule } from './common/common.module';
 import { SeedModule } from './seed/seed.module';
 import { FilesModule } from './files/files.module';
-import { join } from 'path';
-import { ServeStaticModule } from '@nestjs/serve-static';
 import { AuthModule } from './auth/auth.module';
 import { MessagesWsModule } from './messages-ws/messages-ws.module';
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
+
     TypeOrmModule.forRoot({
-      schema: 'public',
       type: 'postgres',
       host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT || '5432'),
       database: process.env.DB_NAME,
       username: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
+      ssl: process.env.STAGE === 'prod'
+        ? { rejectUnauthorized: false }
+        : false,
       autoLoadEntities: true,
       synchronize: true,
     }),
+
     ServeStaticModule.forRoot({
       rootPath: join(__dirname, '..', 'public'),
     }),
+
     ProductsModule,
+
     CommonModule,
+
     SeedModule,
+
     FilesModule,
+
     AuthModule,
+
     MessagesWsModule,
   ],
-  controllers: [],
-  providers: [],
 })
 export class AppModule {}
